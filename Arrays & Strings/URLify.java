56f6d3623e8a303e29058ad3a0b734097ba57a23
Muhammad Usama Ijaz <mijaz@u.rochester.edu>
2020-06-07 15:12:15 -0400

    Added in place function for URLify

--- Arrays & Strings/URLify.java
+++ Arrays & Strings/URLify.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class URLify{
 
 	//Without in place operations
@@ -14,8 +16,34 @@ public class URLify{
 	}
 
 	//In place operations
+	public static String Urlify2(String s, int length){
+		int space = 0;
+		for(char c: s.toCharArray()){
+			if(c == ' '){
+				space++;
+			}
+		}
+		int newLength = length + space*2;
+		Character[] newString = new Character[newLength];
+		int j=0;
+		for(int i = 0; i < length; i++){
+			if(s.charAt(i) == ' '){
+				System.out.println(s.charAt(i) + " "+  j);
+				newString[j] = '%';
+				newString[j+1] = '2';
+				newString[j+2] = '0';
+				j = j+3;
+			}else{
+				System.out.println(s.charAt(i) + " "+  j);
+				newString[j] = s.charAt(i);
+				j++;
+			}
+		}
+		return Arrays.toString(newString);
+	}
 
 	public static void main(String args[]){
 		System.out.println(Urlify("Mr John Smith"));
+		System.out.println(Urlify2("Mr John Smith", 13));
 	}
 }
